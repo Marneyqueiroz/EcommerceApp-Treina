e88d6d5087a8793b13ef0b957bbe2b436417f469
Marneyqueiroz <marney.queiroz@gmail.com>
2023-01-11 19:40:18 -0300

    Criação da Tela Principal

--- src/telas/TelaPrincipal/index.js
+++ src/telas/TelaPrincipal/index.js
@@ -1,15 +1,28 @@
 import React from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity } from "react-native";
 import imagem from "../../../assets/tela_principal.png";
 
 export default function TelaPrincipal() {
     return <>
         <View>
             <Image source={imagem} style={estilos.imagem}></Image>
-            <Text>teste</Text>
+            <Text style={estilos.titulo}>Todas as suas compras num só app!</Text>
+            <Text style={estilos.subtitulo}>Venda seus produtos da maneira mais inteligente e rápida para obter dinheiro imediato e uma consciência mais limpa.</Text>
+
+            <View style = {estilos.botoes}>
+                <TouchableOpacity style = {estilos.botaoLogin}>
+                    <Text style = {estilos.textoBotaoLogin}>Log In</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={estilos.botaoSingUp}>
+                    <Text style = {estilos.textoBotaoSingUp}>Sing Up</Text>
+                </TouchableOpacity>
+            </View>
         </View>
+
     </>
 }
+
 
 const estilos = StyleSheet.create({
 
@@ -17,7 +30,49 @@ const estilos = StyleSheet.create({
         marginTop: 120,
         width: 360,
         height: 360
-
+    },
+    titulo: {
+        margin: 25,
+        marginBottom: 15,
+        fontSize: 35
+    },
+    subtitulo: {
+        margin: 25,
+        marginTop: 0,
+        fontSize: 16
+    },
+    botoes: {
+        marginTop: 25,
+        flexDirection:"row",
+        justifyContent: 'space-between'
+    },
+    botaoLogin:{
+        backgroundColor: "#FF7A00",
+        padding: 10,
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: "#FF7A00",
+        width: 170,
+        marginStart: 25
+    },
+    textoBotaoLogin:{
+        textAlign: "center",
+        color:"#FFFFFF",
+        fontWeight:"bold"
+    },
+    botaoSingUp:{
+        backgroundColor: "#FF7A00",
+        padding: 10,
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: "#FF7A00",
+        width: 170,
+        marginEnd: 25
+    },
+    textoBotaoSingUp:{
+        textAlign: "center",
+        color:"#FFFFFF",
+        fontWeight:"bold"
     }
 
 })
